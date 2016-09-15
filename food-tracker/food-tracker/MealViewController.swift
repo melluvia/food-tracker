b065f5ad2fe9e20e9a40bbe775cf1e1bff09a334
Melissa Phillips <melissanicolephillips@gmail.com>
2016-09-15 16:48:35 -0500

    Added editing capability
    Added the ability to edit a saved meal and save the edits.

--- food-tracker/food-tracker/MealViewController.swift
+++ food-tracker/food-tracker/MealViewController.swift
@@ -26,7 +26,17 @@ class MealViewController: UIViewController, UITextFieldDelegate, UIImagePickerCo
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		
+		// Handle the text field’s user input via delegate callbacks.
 		nameTextField.delegate = self
+		
+		// Set up views if editing an existing Meal.
+		if let meal = meal {
+			
+			navigationItem.title = meal.name
+			nameTextField.text   = meal.name
+			photoImageView.image = meal.photo
+			ratingControl.rating = meal.rating
+		}
 		
 		// Enable the Save button only if the text field has a valid Meal name.
 		checkValidMealName()
