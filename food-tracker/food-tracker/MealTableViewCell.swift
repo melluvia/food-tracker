b5e40eb9e27abd939133ee75b7c010bf840a371c
QuaereVeritatem <robert.lee.martin@gmail.com>
2016-10-19 18:00:53 -0500

    re adding avgStar Labels
    re adding avgStar Labels and headers in your dishes VC, constraints for “your dishes” VC are now fixed..adding meals still causes crashes…will work on in future updates

--- food-tracker/food-tracker/MealTableViewCell.swift
+++ food-tracker/food-tracker/MealTableViewCell.swift
@@ -14,7 +14,7 @@ class MealTableViewCell: UITableViewCell {
 
 	@IBOutlet weak var nameLabel: UILabel!
 	@IBOutlet weak var photoImageView: UIImageView!
-    @IBOutlet weak var avgStarHeader: UILabel! 
+    @IBOutlet weak var avgStarHeader: UILabel! //this doesnt change
 	@IBOutlet weak var ratingControl: RatingControl!
 	@IBOutlet weak var spinner: UIActivityIndicatorView!
     @IBOutlet weak var avgRatingLabel: UILabel!
